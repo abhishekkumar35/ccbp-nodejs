ba809f97aa699ec1adb82a04e45957daa09e4495
Abhishek kumar <flashtoak@gmail.com>
2022-01-31 03:38:05 +0000

    3rd test case failed

--- coding-practices/coding-practice-2a/index.js
+++ coding-practices/coding-practice-2a/index.js
@@ -1,8 +1,9 @@
 const addDays = require("date-fns/addDays");
 module.exports = function (days) {
   const newDate = addDays(new Date(2020, 7, 22), days);
+  return newDate;
   year = newDate.getFullYear();
   month = newDate.getMonth() + 1;
-  day = newDate.getDate();
-  return `${year}-${month}-${day}`;
+  days = newDate.getDate();
+  return `${year}-${month}-${days}`;
 };

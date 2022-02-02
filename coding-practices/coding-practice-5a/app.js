d5e05101a8bfc0dfd98d46d3bffad63d21d11bfb
Abhishek kumar <flashtoak@gmail.com>
2022-02-02 15:02:49 +0000

    developed REST API, 11/11 Test passed

--- coding-practices/coding-practice-5a/app.js
+++ coding-practices/coding-practice-5a/app.js
@@ -13,22 +13,6 @@ const initializeServerAndDatabase = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-<<<<<<< HEAD
-    console.log("Database Connected!");
-    app.listen(3300, () => {
-      console.log(`Server Started and Listening at http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-initializeDatabaseAndServer();
-
-app.get("/movies/", async (request, response) => {
-  try {
-    const fetchListOfMoviesInTable = `select * from movie`;
-    const dbResponse = await db.all(fetchListOfMoviesInTable);
-=======
     console.log("Database connected successfully !");
     app.listen(3000, (error) => {
       if (error) console.log("Server Error!");
@@ -40,12 +24,17 @@ app.get("/movies/", async (request, response) => {
   }
 };
 initializeServerAndDatabase();
+
 app.get("/movies", async (request, response) => {
   try {
     const moviesQ = `select movie_name from movie`;
     const dbResponse = await db.all(moviesQ);
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
-    response.send(dbResponse);
+
+    response.send(
+      dbResponse.map((obj) => {
+        return { movieName: obj.movie_name };
+      })
+    );
   } catch (error) {
     console.log(error);
   }
@@ -54,55 +43,50 @@ app.get("/movies", async (request, response) => {
 app.get("/movies/:movieId/", async (request, response) => {
   try {
     const { movieId } = request.params;
-<<<<<<< HEAD
-    const fetchMovieInTable = `select * from movie
-      where movie_id = ${movieId}`;
-    const dbResponse = await db.get(fetchMovieInTable);
-=======
     console.log(movieId);
     const movieQ = `select * from movie
     where movie_id = ${movieId};`;
     const dbResponse = await db.get(movieQ);
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
-    response.send(dbResponse);
+    response.send({
+      movieId: dbResponse.movie_id,
+      directorId: dbResponse.director_id,
+      movieName: dbResponse.movie_name,
+      leadActor: dbResponse.lead_actor,
+    });
   } catch (error) {
     console.log(error);
   }
 });
 
-<<<<<<< HEAD
-app.get("/directors/", async (request, response) => {
-  try {
-    const fetchDirectorsInTable = `select * from director;`;
-    const dbResponse = await db.all(fetchDirectorsInTable);
-=======
 app.get("/directors/:directorId/movies/", async (request, response) => {
   try {
     const { directorId } = request.params;
     const directorMoviesQ = `select * from movie
     where director_id = ${directorId};`;
     const dbResponse = await db.all(directorMoviesQ);
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
-    response.send(dbResponse);
+    console.log(dbResponse);
+    response.send(
+      dbResponse.map((movie) => {
+        return {
+          movieName: movie.movie_name,
+        };
+      })
+    );
   } catch (error) {
     console.log(error);
   }
 });
 
-<<<<<<< HEAD
-app.get("/directors/:directorId/movies/", async (request, response) => {
-  try {
-    const { director_id } = request.params;
-
-    const fetchMoviesOfDirectorQuery = `select * from movie`;
-    const dbResponse = db.all(fetchMoviesOfDirectorQuery);
-=======
 app.get("/directors", async (request, response) => {
   try {
     const moviesQ = `select * from director`;
     const dbResponse = await db.all(moviesQ);
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
-    response.send(dbResponse);
+
+    response.send(
+      dbResponse.map((e) => {
+        return { directorId: e.director_id, directorName: e.director_name };
+      })
+    );
   } catch (error) {
     console.log(error);
   }
@@ -112,39 +96,17 @@ app.post("/movies/", async (request, response) => {
   try {
     const movieDetails = request.body;
     const { directorId, movieName, leadActor } = movieDetails;
-<<<<<<< HEAD
-    const addNewMovieQuery = `insert into movie 
-    (director_id, movie_name, lead_actor)
-    values
-    (${directorId}, ${movieName}, ${leadActor});
-    `;
-    await db.run(addNewMovieQuery);
-    response.send("Movie Successfully Added");
-=======
     const updateMovieQ = `insert into
     movie 
     (director_id,movie_name,lead_actor) values (${directorId},'${movieName}','${leadActor}')
     ;`;
     await db.run(updateMovieQ);
     response.send(`Movie Successfully Added`);
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
   } catch (error) {
     console.log(error);
   }
 });
 
-<<<<<<< HEAD
-app.put("/movies/:movieId/", async (request, response) => {
-  try {
-    const movieId = request.params();
-    const movieDetails = request.body;
-    const { directorId, movieName, leadActor } = movieDetails;
-    const updateMovieQuery = `update movie set
-    director_id=${directorId}, movie_name= ${movieName}, lead_actor=${leadActor}
-    where movie_id = ${movieId}`;
-    await db.run(updateMovieQuery);
-    response.send("Movie Details Updated");
-=======
 app.put("/movies/:movieId", async (request, response) => {
   try {
     const { movieId } = request.params;
@@ -154,39 +116,27 @@ app.put("/movies/:movieId", async (request, response) => {
     const updateMovieQ = `update
     movie
     set
-        director_id='${directorId}',
-        movie_name=${movieName},
+        director_id=${directorId},
+        movie_name='${movieName}',
         lead_actor='${leadActor}'
     where movie_id = ${movieId};`;
     await db.run(updateMovieQ);
 
     response.send(`Movie Details Updated`);
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
   } catch (error) {
     console.log(error);
   }
 });
 
-<<<<<<< HEAD
-app.delete("/movies/:movieId/", async (request, response) => {
-  try {
-    const movieId = request.params();
-    const deleteMovieQuery = `delete from movie where movie_id = ${movieId}`;
-    await db.run(deleteMovieQuery);
-=======
 app.delete("/movies/:movieId", async (request, response) => {
   try {
     const { movieId } = request.params;
     const deleteMovieQ = `DELETE from movie where movie_id = ${movieId}`;
     await db.run(deleteMovieQ);
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
     response.send("Movie Removed");
   } catch (error) {
     console.log(error);
   }
 });
-<<<<<<< HEAD
-=======
 
 module.exports = app;
->>>>>>> fb164f1d9859155c8e70798223c40ffce14f231c
